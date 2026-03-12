8a46e8b7546beb2717b73e71a24b56cde2f51b3e
nidhi-agrawal-1703 <nidhi.agrawal1703@gmal.com>
2026-03-12 11:52:38 +0530

    12-03-2026

---
+++ project-10/src/main/java/com/rays/common/DropdownList.java
@@ -0,0 +1,8 @@
+package com.rays.common;
+
+public interface DropdownList {
+	
+	public String getKey();
+	
+	public String getvalue();
+}
--- project-10/src/main/java/com/rays/dao/UserDAOImpl.java
+++ project-10/src/main/java/com/rays/dao/UserDAOImpl.java
@@ -1,8 +1,28 @@
 package com.rays.dao;
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import com.rays.common.BaseDAOImpl;
 import com.rays.dto.UserDTO;
 
 public class UserDAOImpl extends BaseDAOImpl<UserDTO> implements UserDAOInt {
+	
+	
+	
+	@Override
+	public Class<UserDTO> getDTOClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected List<Predicate> getWhereClause(UserDTO DTO, CriteriaBuilder builder, Root<UserDTO> qRoot) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
